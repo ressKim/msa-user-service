6d19966af83a13d26025889e67ec29f9ab739ff4
ressKim <sh21dlat@gmail.com>
2021-06-17 18:53:46 +0900

    microservice users 생성

---
+++ src/main/java/com/study/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.study.userservice.controller;
+
+import com.study.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Environment env;
+
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+}
