9e1ba7dfa9e538b8bffc4677d2be80d328e62dbf
ressKim <sh21dlat@gmail.com>
2021-06-19 21:23:08 +0900

    user microservice 와 spring cloud gateway 연동

--- src/main/java/com/study/userservice/controller/UserController.java
+++ src/main/java/com/study/userservice/controller/UserController.java
@@ -23,9 +23,10 @@ public class UserController {
 
     private final Greeting greeting;
 
-    @GetMapping("/health_check")
+    @GetMapping("/user-service/health_check")
     public String status() {
-        return "It's Working in User Service";
+        return String.format("It's Working in User Service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")
